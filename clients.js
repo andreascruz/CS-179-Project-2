4163541ae4d9512b91db21b5628d893ecd1a9266
Khalid Tawil <tawilk2014@gmail.com>
2017-04-19 14:08:07 -0400

    Moved event handlers to html and several bugfixes

--- clients.js
+++ clients.js
@@ -1,18 +1,21 @@
 // A data structure for storing information about our clients
-var clients = [
-  {
+var clients = {
+  "Artemis Cooper" : {
     name: "Artemis Cooper",
     phone: "+1 (012) 345-6789",
     groups: [],
     orders: []
   },
-  {
+  "Benjamin Button" : {
     name: "Benjamin Button",
     phone: "+1 (012) 345-6789",
     groups: [],
-    orders: []
+    orders: [
+      "2017/03/23 (J897QF)",
+      "2017/01/06 (A34DZ6)",
+    ]
   },
-  {
+  "Johnny Appleseed" : {
     name: "Johnny Appleseed",
     phone: "+1 (012) 345-6789",
     groups: [
@@ -26,67 +29,61 @@ var clients = [
       "2017/08/11 (Y965GS)",
     ]
   },
-  {
+  "Jane Anderson" : {
     name: "Jane Anderson",
     phone: "+1 (012) 345-6789",
     groups: [],
     orders: []
   },
-  {
+  "Marcela Hernandez" : {
     name: "Marcela Hernandez",
     phone: "+1 (012) 345-6789",
     groups: [],
     orders: []
   },
-  {
+  "Pauline Dreyfus" : {
     name: "Pauline Dreyfus",
     phone: "+1 (012) 345-6789",
     groups: [],
     orders: []
   },
-  {
+  "Rubio Junipero" : {
     name: "Rubio Junipero",
     phone: "+1 (012) 345-6789",
     groups: [],
     orders: []
   },
-  {
+  "Tatiana Linsker" : {
     name: "Tatiana Linsker",
     phone: "+1 (012) 345-6789",
     groups: [],
     orders: []
   },
-  {
+  "Zsa Zsa Gabor" : {
     name: "Zsa Zsa Gabor",
     phone: "+1 (012) 345-6789",
     groups: [],
     orders: []
   },
-]
-var client = clients[2];
+}
+var client = clients["Johnny Appleseed"];
 
 // Renders a list of all clients
 function displayClients() { //creates the html elements to show the clients
   var pHtml = '<ul id="clientsList" data-role="listview" data-filter="true" data-filter-placeholder="search" data-autodividers="true">';
-  for (var i = 0; i < clients.length; i++) {
-      pHtml += '<li class="clientItem" id="' + i + '"><a href="#appleseed">' + clients[i].name + '</a></li>';
+  for (clientName in clients) {
+      pHtml += '<li class="clientItem"><a href="#appleseed">' + clientName + '</a></li>';
   }
   pHtml += '</ul>';
   $("#clientsList").replaceWith(pHtml);
   $(".p-content").enhanceWithin();
 
-  $(".new-client-save").click(function() {
-    console.log(this);
-    newClient();
-  });
-
   $(".clientItem").click(function() {
-    var cliendId = $(this).attr('id');
-    client = clients[cliendId];
-    console.log('you clicked', clients[cliendId].name);
+    var clientId = $(this)[0].textContent;
+    client = clients[clientId];
     displayClientInfo();
   });
-};
+}
 
 // Renders a specific client page
 function displayClientInfo() { //creates the html elements to show the clients
@@ -99,10 +96,10 @@ function displayClientInfo() { //creates the html elements to show the clients
   for (var j = 0; j < client.groups.length; j++) {
     clientListHtml += '<li>' + client.groups[j] + '</li>';
   }
-  clientListHtml += '<li data-role="list-divider" class="ui-btn ui-icon-plus ui-btn-icon-right">Orders</li>';
+  clientListHtml += '<li data-role="list-divider">Orders</li>';
   // Populates the orders of a client
   for (var j = 0; j < client.orders.length; j++) {
-    clientListHtml += '<li>' + client.orders[j] + '</li>';
+    clientListHtml += '<li onclick="displayNotification()"><a href="#sendNotification">' + client.orders[j] + '</a></li>';
   }
   clientListHtml += '</ul>';
 
@@ -110,36 +107,55 @@ function displayClientInfo() { //creates the html elements to show the clients
   $(".customer-info2").html(clientInfoHtml);
   $('.group-list').replaceWith(clientListHtml);
   $(".p-content").enhanceWithin();
-};
+}
+
+// Display personalized notifications
+function displayNotification() {
+  console.log('displaying notification');
+  noteInfo = '<div id="sendNotificationClientInfo">';
+  noteInfo += '<p>' + client.name + '</p>';
+  noteInfo += '<p>' + client.phone + '</p>';
+  $('#sendNotificationClientInfo').replaceWith(noteInfo);
+}
 
 // Adds a new client to our array
 function newClient() {
+  console.log('creating new client');
   var firstName = $('#new-first-name')[0].value;
   var lastName = $('#new-last-name')[0].value;
+  var fullName = firstName + " " + lastName;
   var phone = $('#new-phone')[0].value;
-  newClient = {
-    name: firstName + " " + lastName,
+  var checkbox = $(':checked');
+  var checkedGroups = [];
+  for (var i = 0; i < checkbox.length; i++) {
+    var cId = checkbox[i].id;
+    var checkedGroup = $('label[for=' + cId + ']')[1].innerHTML;
+    checkedGroups = checkedGroups.concat(checkedGroup);
+  }
+  client2 = {
+    name: fullName,
     phone: phone,
-    groups: [],
+    groups: checkedGroups,
     orders: []
   };
-  console.log(newClient);
-  clients = clients.concat(newClient);
+  clients[fullName] = client2;
   displayClients();
-};
+}
 
-$( function() {
-    var availableTags = [];
-    for(i = 0; i < clients.length; i++)
-    {
-      availableTags = availableTags.concat(clients[i].name);
-    }
-    $( "#tags" ).autocomplete({
-      source: availableTags
-    });
-  } );
+function autoType() {
+  var availableTags = [];
+  for(i = 0; i < clients.length; i++)
+  {
+    availableTags = availableTags.concat(clients[i].name);
+  }
+  $( "#tags" ).autocomplete({
+    source: availableTags
+  });
+}
 
 $(function() {
   console.log('Fresh start!');
+
   displayClients(); //generate and render the html divs for the clients list
+  autoType();
 });
